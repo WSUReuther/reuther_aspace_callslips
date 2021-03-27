2bb5a2115b691fcb16a56d46456c66a357337ce7
Dallas Pillen <dallaspillen@gmail.com>
2021-03-27 11:51:32 -0400

    open window before ajax to avoid popup blocks

--- frontend/assets/add_call_slip.js
+++ frontend/assets/add_call_slip.js
@@ -8,6 +8,7 @@ $(function () {
 
   var add_call_slip = function () {
     var $object_uri = $("#uri").attr("value");
+    var $call_slip_window = window.open("", "_blank");
     $.ajax({
       url: AS.app_prefix("/plugins/call_slips/generate"),
       data: {
@@ -15,7 +16,7 @@ $(function () {
       },
       type: "POST",
       success: function (call_slip_url) {
-        window.open(call_slip_url);
+        $call_slip_window.location = call_slip_url;
       },
     })
   };
